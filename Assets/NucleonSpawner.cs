bda1281bb7cc1e2db1b8981d2ebb8e140321ca80
Jonathan Maurice <jonathanma@unity3d.com>
2019-11-02 22:02:24 -0400

    Initial files

---
+++ Assets/NucleonSpawner.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class NucleonSpawner : MonoBehaviour
+{
+    public float timeBetweenSpawns;
+
+    public float spawnDistance;
+
+    public Nucleon[] nucleonPrefabs;
+    
+    float timeSinceLastSpawn;
+
+    List<Nucleon> m_Nucleons = new List<Nucleon>();
+
+    void FixedUpdate ()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        if (timeSinceLastSpawn >= timeBetweenSpawns)
+        {
+            timeSinceLastSpawn -= timeBetweenSpawns;
+            SpawnNucleon();
+        }
+    }
+    
+    void SpawnNucleon () 
+    {
+        Nucleon prefab = nucleonPrefabs[Random.Range(0, nucleonPrefabs.Length)];
+        Nucleon spawn = Instantiate<Nucleon>(prefab);
+        spawn.transform.position = Random.onUnitSphere * spawnDistance;
+        
+        m_Nucleons.Add(spawn);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach (var nucleon in m_Nucleons)
+            {
+                nucleon.enabled = false;
+            }
+            m_Nucleons.Clear();
+        }
+    }
+}
